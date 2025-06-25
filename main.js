d0a2d3b7d647c76fa191ea68fae811e0c2b21a57
caio594 <candrerosadossantos@gmail.com>
2025-06-25 20:15:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+import ui from "./app.js";
+import api from "./api.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.renderizarClientes();
+    const formularioCliente = document.getElementById('form-clientes');
+    formularioCliente.addEventListener('submit', manipularSubForm);
+});
+
+async function manipularSubForm(evento) {
+    evento.preventDefault();
+    const id = document.getElementById('cliente_id').value;
+    const email = document.getElementById('cliente_email').value;
+    const nome = document.getElementById('cliente_nome').value;
+    const sobrenome = document.getElementById('cliente_sobrenome').value;
+    const celular = document.getElementById('cliente_celular').value;
+    const cidade = document.getElementById('cliente_cidade').value;
+    const rua = document.getElementById('cliente_rua').value;
+    const cpf = document.getElementById('cliente_cpf').value;
+    try {
+        if(id){
+            await api.editarcliente({ id, email, nome, sobrenome, celular, cidade, rua, cpf });
+        }else{
+            await api.cadastrarNovocliente({ email, nome, sobrenome, celular, cidade, rua, cpf });
+        }
+        ui.renderizarClientes();
+    } catch (error) {
+        alert('Erro ao salvar novo cliente')
+    }
+
+}
